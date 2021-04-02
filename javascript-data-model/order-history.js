8d9285ba1d294e247f1614ed69403477ebc898b7
Fernando Figueiredo <bookaneers@gmail.com>
2021-04-02 15:36:31 -0700

    Updated and completed javascript-data-model.

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,10 +1,11 @@
 var orderHistory = [
   {
-    orderNumber: '001',
-    orderPlacedAt: '03 / 16 / 2021',
-    deliveredOn: '04 / 01 / 21',
-    totalForTheOrder: 80.00,
+    orderNumber: '114-3941689-8772232',
+    orderPlacedAt: '08 / 04 / 2020',
+    deliveredOn: '08 / 08 / 2020',
+    totalForTheOrder: 31.55,
     shipTo: {
+      customer: 'JS Masher',
       line1: '364 40th Street',
       line2: 'Apt 401',
       city: 'Oakland',
@@ -12,16 +13,80 @@ var orderHistory = [
       zip: 94609
     },
     itemsOrdered: [{
-      isbn: 124567890123,
-      author: 'Smith',
-      title: 'JS for Beginners',
-      price: 19.99,
+      isbn: 124567890121,
+      author: 'Dr. Axel',
+      title: 'JavaScript for impatient programmers',
+      price: 31.55,
       coverImage: 'image'
-    }, {
+    }]
+  },
+  {
+    orderNumber: '113-9984268-1280257',
+    orderPlacedAt: '07 / 19 / 2020',
+    deliveredOn: '07 / 20 / 2020',
+    totalForTheOrder: 41.33,
+    shipTo: {
+      customer: 'JS Masher',
+      line1: '364 40th Street',
+      line2: 'Apt 401',
+      city: 'Oakland',
+      state: 'CA',
+      zip: 94609
+    },
+    itemsOrdered: [{
+      isbn: 124567890122,
+      author: 'Alexander, Christopher',
+      title: 'The Timeless Way of Building',
+      price: 41.33,
+      coverImage: 'image'
+    }]
+  },
+  {
+    orderNumber: '114-2875557-9059409',
+    orderPlacedAt: '07 / 04 / 2020',
+    deliveredOn: '07 / 07 / 2020',
+    totalForTheOrder: 15.98,
+    shipTo: {
+      customer: 'JS Masher',
+      line1: '364 40th Street',
+      line2: 'Apt 401',
+      city: 'Oakland',
+      state: 'CA',
+      zip: 94609
+    },
+    itemsOrdered: [{
       skuNumber: 1234,
-      productDescription: 'GameX',
+      productDescription: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
       manufacturer: 'Nintendo',
-      price: 59.99,
+      price: 15.98,
+      coverImage: 'image'
+    }]
+  },
+  {
+    orderNumber: '113-2883177-2648248',
+    orderPlacedAt: '07 / 03 / 2020',
+    deliveredOn: '07 / 05 / 2020',
+    totalForTheOrder: 138.93,
+    shipTo: {
+      customer: 'JS Masher',
+      line1: '364 40th Street',
+      line2: 'Apt 401',
+      city: 'Oakland',
+      state: 'CA',
+      zip: 94609
+    },
+    itemsOrdered: [{
+      skuNumber: 1267,
+      productDescription: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+      manufacturer: 'Nintendo',
+      price: 94.95,
+      coverImage: 'image'
+    },
+    {
+      isbn: 124567890123,
+      author: 'Faroult, Stephane',
+      title: 'The Art of Sql',
+      price: 33.99,
       coverImage: 'image'
     }]
   }
