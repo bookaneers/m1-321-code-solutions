3aea6521806b37c78407f29b89f8b70bce389cd9
Fernando Figueiredo <bookaneers@gmail.com>
2021-04-15 20:16:48 -0700

    Completed dom-querying.

--- dom-querying/main.js
+++ dom-querying/main.js
@@ -10,3 +10,9 @@ console.dir(el2);
 var el3 = document.querySelector('.hint');
 console.log(el3);
 console.dir(el3);
+
+var el4 = document.querySelectorAll('p');
+console.log(el4);
+
+var el5 = document.querySelectorAll('.example-link');
+console.log(el5);
