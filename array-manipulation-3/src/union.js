fc4a76423fc587f6842ce2f78610bc6a8d534c8d
Fernando Figueiredo <bookaneers@gmail.com>
2021-04-29 16:03:49 -0700

    Completed array-manipulation-3.

---
+++ array-manipulation-3/src/union.js
@@ -0,0 +1,11 @@
+/* exported union */
+function union(first, second) {
+  var result = [];
+  for (var i = 0; i < first.length; i++) {
+    if (!result.includes(first[i])) result.push(first[i]);
+  }
+  for (var k = 0; k < second.length; k++) {
+    if (!result.includes(second[k])) result.push(second[k]);
+  }
+  return result;
+}
