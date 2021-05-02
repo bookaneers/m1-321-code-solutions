35fa7a522c8d3f59da45e8d91480d3855f85897b
Fernando Figueiredo <bookaneers@gmail.com>
2021-05-02 13:32:00 -0700

    Completed string-manipulation-3.

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,1 +1,11 @@
 /* exported reverseWords */
+function reverseWords(string) {
+  var arrayOfWords = string.split(' ');
+  var wordOfArray = '';
+  var newArray = [];
+  for (var i = 0; i < arrayOfWords.length; i++) {
+    wordOfArray = arrayOfWords[i];
+    newArray.push(wordOfArray.split('').reverse().join(''));
+  }
+  return newArray.join(' ');
+}
